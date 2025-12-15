f9cb04bfb6be4a79308535ab8ed2ed141d9e34ab
Nate Langhinrichs <nate.langhinrichs@gmail.com>
2025-12-14 22:10:59 -0500

    working small-scale, not large

--- 09/09.py
+++ 09/09.py
@@ -17,11 +17,42 @@ for i in range(len(data)):
 print(maximum)
 
 print("Part 2:")
+
+left_candidates = []
+right_candidates = []
+lefts = 0
+rights = 0
+for i in range(len(data)):
+    prev_pt = data[i-1]
+    current_pt = data[i]
+    next_pt = data[(i+1)%len(data)]
+    v_1 = [current_pt[0]-prev_pt[0], current_pt[1]-prev_pt[1]]
+    v_2 = [next_pt[0]-current_pt[0], next_pt[1]-current_pt[1]]
+
+    delta = [-v_1[0]/abs(sum(v_1))+v_2[0]/abs(sum(v_2)), -v_1[1]/abs(sum(v_1))+v_2[1]/abs(sum(v_2))]
+    candidate = (int(current_pt[0] + delta[0]), int(current_pt[1]+delta[1]))
+    anti_candidate = (int(current_pt[0] - delta[0]), int(current_pt[1] - delta[1]))
+    if v_1[0]*v_2[1] > v_1[1]*v_2[0]: # cross-product, right hand rule
+        right_candidates.append(candidate)
+        left_candidates.append(anti_candidate)
+        rights += 1
+    else:
+        left_candidates.append(candidate)
+        right_candidates.append(anti_candidate)
+        lefts += 1
+if rights > lefts: #going clockwise, so right-candidates are interior
+    candidates = right_candidates
+    anti = left_candidates
+else: #going counterclockwise, this set are interior
+    candidates = left_candidates
+    anti = right_candidates
+print(candidates)
+
 width = max(data, key=lambda x: x[0])[0]+2
-min_x = min(data, key=lambda x: x[0])[0]-1
+#min_x = min(data, key=lambda x: x[0])[0]
 height = max(data, key=lambda x: x[1])[1]+2
-min_y = min(data, key=lambda x: x[1])[1]-1
-domain = [['.' for i in range(min_x, width)] for j in range(min_y, height)] 
+#min_y = min(data, key=lambda x: x[1])[1]-1
+domain = [['.' for i in range(width)] for j in range(height)] 
 
 for i in range(len(data)):
     d = data[i]
@@ -33,37 +64,17 @@ for i in range(len(data)):
             domain[data[i][1]][j] = 'x'
     x, y = d
     domain[y][x] = '#'
-pprint.pprint(domain)
-
-seed = (0, 0)
-domain[0][0] = '-'
-flood = [(0, 1), (1, 0)]
-checked = [(0,0)]
-while len(flood) > 0:
-    f = flood[0]
-    if domain[f[1]][f[0]] not in ['-', '#', 'x']:
-        domain[f[1]][f[0]] = '-'
-        checked.append(f)
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                test = (f[0]+i, f[1]+j)
-                if test not in checked:
-                    flood.append(test)
-    flood = flood[1:]
-
+for c in candidates:
+    domain[c[1]][c[0]]= 'c'
+for a in anti:
+    domain[a[1]][a[0]] = 'a'
 pprint.pprint(domain)
 
 maximum = 0
 for i in range(len(data)):
     for j in range(i+1, len(data)):
-        still_in = True
         area = (abs(data[i][0]-data[j][0])+1)*(abs(data[i][1]-data[j][1])+1)
-        if area > maximum:
-            for x in range(min(data[i][0], data[j][0]), max(data[i][0], data[j][0])+1):
-                for y in range(min(data[i][1], data[j][1]), max(data[i][1], data[j][1])+1):
-                    if domain[y][x] not in ['#', 'x', '.']:
-                        still_in = False
-                        break
-            if still_in:
-                maximum = area
+        conflicts = [a for a in anti if (a[0]-data[i][0])*(a[0]-data[j][0])<0 and (a[1]-data[i][1])*(a[1]-data[j][1])<0]
+        if conflicts == [] and area > maximum:
+            maximum = area
 print(maximum)
